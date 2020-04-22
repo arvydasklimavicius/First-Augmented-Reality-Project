7a844f5772f856d37d1ab0677cb9707bf534d007
Arvydas Klimavicius <arvydas.klimavicius@icloud.com>
2020-04-22 16:29:19 +0300

    Cube Node added and make spinning

--- ViewController.swift
+++ ViewController.swift
@@ -16,18 +16,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
+        sceneView.debugOptions = [.showFeaturePoints, .showWorldOrigin]
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.autoenablesDefaultLighting = true
+
+        let cube  = SCNBox(width: 0.3, height: 0.3, length: 0.3, chamferRadius: 0.0)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.green
+        cube.materials = [material]
+
+        let cubeNode = SCNNode()
+        cubeNode.geometry = cube
+        cubeNode.position = SCNVector3(0.0, 0.0, -0.5)
+
+        let rotation = SCNAction.rotateBy(x: 0, y: 2 * .pi, z: 0, duration: 2)
+        let repeatRotatioin = SCNAction.repeatForever(rotation)
+        cubeNode.runAction(repeatRotatioin)
+        sceneView.scene.rootNode.addChildNode(cubeNode)
+
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -47,29 +54,4 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
